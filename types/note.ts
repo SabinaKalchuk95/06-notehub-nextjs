52ebefe82e88a31716dd4fa5b28d1a1b6c5c8404
Sabina KAlchuk <ssabki229@gmail.com>
2025-08-04 12:53:38 +0200

    errornote

--- types/note.ts
+++ types/note.ts
@@ -4,7 +4,7 @@ export interface Note {
     id: string;
     title: string;
     content: string;
-    categoryId: NoteTag;
+    tag: NoteTag;
     userId: string;
     createdAt: string;
     updatedAt: string;
